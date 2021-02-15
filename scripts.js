05cccddb1ec081f044feb3f9f3e2c018d7671cad
Your Name <you@example.com>
2021-02-15 14:33:12 +0600

    вщсгьутеюцкшеу удуьутеы

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 let fruits = ["Яблоко", "Апельсин", "Слива"];
+
+let ul = document.createElement('ul');
+document.body.append (ul);
+
 for (let i = 0 ; i >  fruits.length; i++){
+   let fruits = document('fruits').innerText = fruits;
 }
-   alert = (fruits[0]);
+   alert = (fruits[2]);
 
