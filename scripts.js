c86dcda9b3a0d2ae5bee4636cacaddd156a72fcc
Your Name <you@example.com>
2021-02-15 16:02:50 +0600

    added files

--- scripts.js
+++ scripts.js
@@ -1,48 +1,46 @@
-let fruits = ['Apple', 'Banana', 'Orange', 'Lemon'];
+let fruits = ["Limon", "Lime", "Apple", "Banana", '', '   '];
 
-let ul = document.createElement('ul');
+let ul = document.createElement("ul");
 let input = document.querySelector("input");
-let btn = document.querySelector("button");
+let button = document.querySelector("button");
 
-document.body.prepend(ul);
-
-btn.addEventListener('click', addElement);
-function addElement(){
-    let li = document.createElement("li");
-    let btn = document.createElement("button");
-   //  let span = document.createElement('span');
-    btn.innerText = "Remove";
-    btn.addEventListener('click', function(){
-        li.remove();
-    });
-    
-    li.innerText = input.value;
-    li.append(btn);
-    ul.append(li);
-    input.value = "";
-   //  li.append(span);
-}
-
-
+document.body.append(ul);
 for (let i in fruits) {
-    let li = document.createElement("li");
-    li.innerText = fruits[i];
-    let btn = document.createElement("button");
-    let span = document.createElement('span');
-    span.addEventListener('click', function(){
-        li.append(span)
-    })
-    let span2 = document.createElement('.selected');
-    span2.addEventListener('click', function(){
-        li.remove(span2)
-    });
-    btn.innerText = "Remove";
-    btn.addEventListener('click', function(){
-        li.remove();
-    });
-    li.append(btn);
-    ul.append(li);
-    li.append(span);
+    let li = returnListItem(fruits[i]);
+    if (li) {
+        ul.append(li);
+    }
 }
 
+button.addEventListener("click", function () {
+    let li = returnListItem(input.value);
+    if (li) {
+        input.value = '';
+        ul.append(li);
+    }
+});
 
+function returnListItem(text = '') {
+    if (!text.trim()) {
+        return false;
+    }
+
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    let span = document.createElement("span");
+
+    button.innerText = "-";
+    span.innerText = text;
+
+    button.addEventListener("click", function () {
+        li.remove();
+    });
+    span.addEventListener("click", function () {
+        span.classList.toggle('done');
+    });
+
+    li.append(span);
+    li.append(button);
+    
+    return li;
+}
