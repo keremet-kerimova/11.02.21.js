31a6fdbf84b3701fbbbb4ca3c011e2ba930b5391
Your Name <you@example.com>
2021-02-15 14:51:27 +0600

    added

--- scripts.js
+++ scripts.js
@@ -1,24 +1,36 @@
-let fruits = ["Яблоко", "Апельсин", "Слива"];
+// let fruits = ["Яблоко", "Апельсин", "Слива"];
 
-let ul = document.createElement('ul');
-document.body.append (ul);
+// let ul = document.createElement('ul');
+// document.body.append (ul);
 
-for (let i = 0 ; i >  fruits.length; i++){
-   let fruits = document('fruits').innerText = fruits;
-}
-   alert = (fruits[2]);
+// for (let i = 0 ; i < fruits.length; i++){
+//    let  li = document.createElement('li').innerText = fruits[i];
+//    ul.append(li);
+// }
+// document.body.append(ul);
+// let input = querySelector("input");
+// let buttom = querySelector("buttom");
 
+// buttom.addEventListiner('click', function(){
+//    let li = document.createElement('li');
+//    li.innerText = input.value
+//    ul.append(li);
+//    input.value = ""
+// });
 
-
-
-
-
-let input = querySelector('input');
-let buttom = querySelector('buttom');
-
-buttom.addEventListiner('click', function(){
-   let li = document.createElement('li');
-   li.innerText = input.value
-   ul.append(li);
-   input.value = ""
+let fruits = ["apple" + " " +  "corret " + " " + "current" + " " + "banana" + " " + "potato",];
+let ul = document.createElement('ul')
+document.body.append(ul);
+for  ( let  i = 0; i<fruits.length; i++ ){
+    let li = document.createElement('li').innerText = fruits[i]
+    ul.append(li);
+};
+document.body.append(ul)
+let input  =document.querySelector("input");
+let button = document.querySelector("button");
+button.addEventListener('click',  function () {
+    let li = document.createElement('li');
+    li.innerText= input.value
+    ul.append(li);
+    input.value= "";
 });
